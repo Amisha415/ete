907bc0b20568b9686932e093431db41ed5606926
Amisha415 <amisha1813.be20@chitkara.edu.in>
2022-10-19 11:07:45 +0530

    f

--- src/components/categories/index.jsx
+++ src/components/categories/index.jsx
@@ -10,8 +10,8 @@ export default function Index() {
                 <div className="2xl:mx-auto 2xl:container py-12 px-4 sm:px-6 xl:px-20 2xl:px-0 w-full">
                     <div className="flex flex-col jusitfy-center items-center space-y-10">
                         <div className="flex flex-col justify-center items-center space-y-2">
-                            <p className="text-xl leading-5 text-gray-600">2022 Trendsetters</p>
-                            <h1 className="text-3xl xl:text-4xl font-semibold leading-7 xl:leading-9 text-gray-800">Shop By Category</h1>
+                            <p className="text-xl leading-5 text-gray-600">ACCORDING TO  2022 TREND</p>
+                            <h1 className="text-3xl xl:text-4xl font-semibold leading-7 xl:leading-9 text-gray-800">E COMMERCE SITE</h1>
                         </div>
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-8 w-full">
                             <Link to="/women" className="relative group flex justify-center items-center h-full w-full">
