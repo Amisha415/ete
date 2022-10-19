b0294231577fa8e6626d13d01e4adc8b7b693663
Amisha415 <amisha1813.be20@chitkara.edu.in>
2022-10-19 10:29:46 +0530

    f

--- src/components/categories/index.jsx
+++ src/components/categories/index.jsx
@@ -26,7 +26,7 @@ export default function Index() {
                                     <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50" />
                                 </Link>
                                 <Link to="/watches" className="relative group flex justify-center items-center h-full w-full">
-                                    <img className="object-center object-cover h-full w-full" src="https://i.ibb.co/Hd1pVxW/louis-mornaud-Ju-6-TPKXd-Bs-unsplash-1-2.png" alt="watch-image" />
+                                    <img className="object-center object-cover h-full w-full" src="https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?auto=compress&cs=tinysrgb&w=600" alt="watch-image" />
                                     <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bottom-4  absolute text-base font-medium leading-none text-gray-800 py-3 w-36 bg-white">Watches</button>
                                     <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50" />
                                 </Link>
